c15d7e8aa9afa835b228b969445fff5aa645f307
Cosmin070 <61682964+Cosmin070@users.noreply.github.com>
2020-05-10 19:11:04 +0300

    modified script

--- scripts/imageclassification.py
+++ scripts/imageclassification.py
@@ -1,25 +1,20 @@
-# TODO: rewrite run and create array to support both sides at the same time -> array[]
+# Version 1.1
 # TODO: write test ->
-# TODO: add unhealthy to right/left
-# TODO: add healthy to caverns/pleurisy
-# TODO: create directories for .npy files
 import csv
 import datetime
 import os
 
 import numpy as np
-from joblib import dump
+from joblib import dump, load
 from keras.preprocessing import image
 from sklearn import svm
 
-from constants import _train_set_unequal, _train_set
+from constants import image_files, label_files
 
-path_left = \
-    'D:\\JetBrains\\FII\\Software-Engineering-2020-Pre-Processing\\Software-Engineering-2020-Pre-Processing\\Left'
-path_right = \
-    'D:\\JetBrains\\FII\\Software-Engineering-2020-Pre-Processing\\Software-Engineering-2020-Pre-Processing\\Right'
-path_test = \
-    'D:\\JetBrains\\FII\\Software-Engineering-2020-Pre-Processing\\Software-Engineering-2020-Pre-Processing\\Test'
+paths = ['D:\\JetBrains\\FII\\Git\\Software-Engineering-2020\\Train',
+         'D:\\JetBrains\\FII\\Git\\Software-Engineering-2020\\Test\\Left']
+
+target = 'D:\\JetBrains\\FII\\IP-2\\target'
 
 
 def count():
@@ -36,133 +31,150 @@ def count():
                 if i[j] == '1':
                     count1 += 1
             result.append([count0, count1])
-        print(result)
+        return result
 
 
-def separate():
+def separate(counts):
     with open('CT_Report.csv', newline='') as patients:
-        counts = [[53, 135], [36, 152], [153, 35], [138, 50], [183, 5], [178, 10]]
         reader = csv.reader(patients, delimiter=',')
         reader = list(reader)
+        result = []
         for i in range(len(counts)):
             counter = min(counts[i])
             unaffected = []
             affected = []
             for row in reader:
-                if row[i + 1] == '1' and len(affected) < counter + 10:
-                    affected.append(row[0])
-                if row[i + 1] == '0' and len(unaffected) < counter:
-                    unaffected.append(row[0])
-            print([unaffected, affected])
+                if 0 <= i < 2:
+                    if row[i + 1] == '1' and len(affected) < counter + 15:
+                        affected.append(row[0])
+                    if row[i + 1] == '0' and len(unaffected) < counter:
+                        unaffected.append(row[0])
+                if 2 <= i < 4:
+                    if row[i + 1] == '1' and len(affected) < counter:
+                        affected.append(row[0])
+                    if row[i + 1] == '0' and len(unaffected) < counter + 15:
+                        unaffected.append(row[0])
+                if 4 <= i < 6:
+                    if row[i + 1] == '1' and len(affected) < counter:
+                        affected.append(row[0])
+                    if row[i + 1] == '0' and len(unaffected) < counter + 10:
+                        unaffected.append(row[0])
+            result.append([affected, unaffected])
+        return result
 
 
-def run():
-    right_image_data = np.load('equal_processed-right.npy')
-    right_caverns_image_data = np.load('equal_processed-right-caverns.npy')
-    right_pleurisy_image_data = np.load('equal_processed-right-pleurisy.npy')
-    labels_right = np.load('equal_right-lung-labels.npy')
-    labels_caverns_right = np.load('equal_right-caverns-labels.npy')
-    labels_pleurisy_right = np.load('equal_right-pleurisy-labels.npy')
-    svm_classifier = svm.SVC(kernel='rbf', C=1.0)
-    svm_classifier2 = svm.SVC(kernel='rbf', C=1.0)
-    svm_classifier3 = svm.SVC(kernel='rbf', C=1.0)
-    svm_classifier.fit(right_image_data, labels_right)
-    print('Fitted')
-    svm_classifier2.fit(right_caverns_image_data, labels_caverns_right)
-    print('Fitted')
-    svm_classifier3.fit(right_pleurisy_image_data, labels_pleurisy_right)
-    print('Fitted')
-    dump(svm_classifier, 'equal_right-learn.joblib')
-    dump(svm_classifier2, 'equal_right-caverns-learn.joblib')
-    dump(svm_classifier3, 'equal_right-pleurisy-learn.joblib')
-    print('Saved')
-    for file in os.listdir(path_test):
-        test_image_data = []
-        root, ext = os.path.splitext(file)
-        _path = os.path.join(path_right, root)
-        for _file in os.listdir(_path):
-            __path = os.path.join(_path, _file)
-            for __file in os.listdir(__path):
-                img = image.load_img(os.path.join(__path, __file), target_size=(32, 32))
-                img = image.img_to_array(img)
-                test_image_data.append(img)
-        test_image_data = np.array(test_image_data, dtype='float32') / 255.0
-        m = test_image_data.shape[0]
-        test_image_data = test_image_data.reshape(m, -1)
-        ypred = svm_classifier.predict(test_image_data)
-        ypred2 = svm_classifier2.predict(test_image_data)
-        ypred3 = svm_classifier3.predict(test_image_data)
-        print(file, end=" ")
-        print(round(np.count_nonzero(ypred == '1') / (
-                np.count_nonzero(ypred == '1') + np.count_nonzero(ypred == '0')), 2), end=" ")
-        print(round(np.count_nonzero(ypred2 == '1') / (
-                np.count_nonzero(ypred2 == '1') + np.count_nonzero(ypred2 == '0')), 2), end=" ")
-        print(round(np.count_nonzero(ypred3 == '1') / (
-                np.count_nonzero(ypred3 == '1') + np.count_nonzero(ypred3 == '0')), 2))
+def get_column(side, region):
+    regions = [[1, 3, 5], [2, 4, 6]]
+    return regions[side][region]
 
 
-def create_arrays():
+def create_arrays(path, counts):
     with open('CT_Report.csv', newline='') as patients:
-        image_data_right = []
-        image_data_right_pleurisy = []
-        image_data_right_caverns = []
-        labels_right = []
-        labels_caverns_right = []
-        labels_pleurisy_right = []
         reader = csv.reader(patients, delimiter=',')
         reader = list(reader)
-        for file in os.listdir(path_right):
-            root, ext = os.path.splitext(file)
-            if file in _train_set[1][0] or file in _train_set[1][1]:
-                _path = os.path.join(path_right, root)
-                for _file in os.listdir(_path):
-                    __path = os.path.join(_path, _file)
-                    for __file in os.listdir(__path):
-                        img = image.load_img(os.path.join(__path, __file), target_size=(32, 32))
-                        img = image.img_to_array(img)
-                        image_data_right.append(img)
-                        labels_right.append(reader[int(file)][2])
-            if file in _train_set[3][0] or file in _train_set[3][1]:
-                _path = os.path.join(path_right, root)
-                for _file in os.listdir(_path):
-                    __path = os.path.join(_path, _file)
-                    for __file in os.listdir(__path):
-                        img = image.load_img(os.path.join(__path, __file), target_size=(32, 32))
-                        img = image.img_to_array(img)
-                        image_data_right_caverns.append(img)
-                        labels_caverns_right.append(reader[int(file)][4])
-            if file in _train_set[5][0] or file in _train_set[5][1]:
-                _path = os.path.join(path_right, root)
-                for _file in os.listdir(_path):
-                    __path = os.path.join(_path, _file)
-                    for __file in os.listdir(__path):
-                        img = image.load_img(os.path.join(__path, __file), target_size=(32, 32))
-                        img = image.img_to_array(img)
-                        image_data_right_pleurisy.append(img)
-                        labels_pleurisy_right.append(reader[int(file)][6])
-        image_data_right = np.array(image_data_right, dtype='float32') / 255.0
-        image_data_right_caverns = np.array(image_data_right_caverns, dtype='float32') / 255.0
-        image_data_right_pleurisy = np.array(image_data_right_pleurisy, dtype='float32') / 255.0
-        labels_right = np.array(labels_right)
-        labels_caverns_right = np.array(labels_caverns_right)
-        labels_pleurisy_right = np.array(labels_pleurisy_right)
-        m = image_data_right.shape[0]
-        image_data_right = image_data_right.reshape(m, -1)
-        m = image_data_right_caverns.shape[0]
-        image_data_right_caverns = image_data_right_caverns.reshape(m, -1)
-        m = image_data_right_pleurisy.shape[0]
-        image_data_right_pleurisy = image_data_right_pleurisy.reshape(m, -1)
-        np.save('equal_processed-right.npy', image_data_right)
-        np.save('equal_processed-right-caverns.npy', image_data_right_caverns)
-        np.save('equal_processed-right-pleurisy.npy', image_data_right_pleurisy)
-        np.save('equal_right-lung-labels.npy', labels_right)
-        np.save('equal_right-caverns-labels.npy', labels_caverns_right)
-        np.save('equal_right-pleurisy-labels.npy', labels_pleurisy_right)
+        file_name = ['left', 'right']
+        region_type = ['lung', 'caverns', 'pleurisy']
+        file_ext = ['fb', 'lr', 'tb']
+        final_files = ['%s-%s.npy' % (x, y) for x in file_name for y in file_ext]
+        final_label_files = ['%s-%s-%s.npy' % (x, y, z) for x in file_name for y in file_ext for z in region_type]
+        image_data = [[[], [], []], [[], [], []]]
+        labels = [[[[], [], []], [[], [], []], [[], [], []]], [[[], [], []], [[], [], []], [[], [], []]]]
+        for file in os.listdir(path[0]):
+            file_path = os.path.join(path[0], file)
+            side = 0 if file == 'Left' else 1
+            for _file in os.listdir(file_path):
+                _path = os.path.join(file_path, _file)
+                for dirs in os.listdir(_path):
+                    region = 0 if dirs == 'FrontBack' else 1 if dirs == 'LeftRight' else 2
+                    if _file in counts[3 * side + region][0] or _file in counts[3 * side + region][1]:
+                        __path = os.path.join(_path, dirs)
+                        for __file in os.listdir(__path):
+                            img = image.img_to_array(image.load_img(os.path.join(__path, __file), target_size=(32, 32)))
+                            image_data[side][region].append(img)
+                            for i in range(3):
+                                labels[side][region][i].append(reader[int(_file) - 1][get_column(side, i)])
+        for side in range(len(image_data)):
+            for region in range(len(image_data[side])):
+                data = np.array(image_data[side][region], dtype='float32') / 255.0
+                m = data.shape[0]
+                data = data.reshape(m, -1)
+                np.save(os.path.join(target, final_files[3 * side + region]), data)
+                for i in range(3):
+                    label = np.array(labels[side][region][i])
+                    np.save(os.path.join(target, 'labels', final_label_files[9 * side + 3 * region + i]), label)
 
 
-print(datetime.datetime.now())
-#create_arrays()
-#run()
-# separate()
-count()
-print(datetime.datetime.now())
+def run(saved=False):
+    print('Fitting...')
+    svm_array = [[[[], [], []], [[], [], []], [[], [], []]], [[[], [], []], [[], [], []], [[], [], []]]]
+    if saved:
+        for i in range(2):
+            for j in range(3):
+                for k in range(3):
+                    svm_array[i][j][k] = svm.SVC(kernel='rbf', C=1.0)
+        for i in range(2):
+            for j in range(3):
+                for k in range(3):
+                    svm_array[i][j][k] = load(str(i) + str(j) + str(k) + '.joblib')
+    else:
+        image_data = [[[], [], []], [[], [], []]]
+        labels = [[[[], [], []], [[], [], []], [[], [], []]], [[[], [], []], [[], [], []], [[], [], []]]]
+        for i in range(2):
+            for j in range(3):
+                image_data[i][j] = np.load(os.path.join(target, image_files[3 * i + j]))
+                for z in range(3):
+                    labels[i][j][z] = np.load(os.path.join(target, "labels", label_files[9 * i + 3 * j + z]))
+        for i in range(2):
+            for j in range(3):
+                for k in range(3):
+                    svm_array[i][j][k] = svm.SVC(kernel='rbf', C=1.0)
+        for i in range(2):
+            for j in range(3):
+                for k in range(3):
+                    svm_array[i][j][k].fit(image_data[i][j], labels[i][j][k])
+                    dump(svm_array[i][j][k], str(i) + str(j) + str(k) + '.joblib')
+    print('Fitted')
+    out = open('results.csv', 'w', newline='')
+    fields = ['Filename', 'LeftLungAffected', 'RightLungAffected', 'CavernsLeft', 'CavernsRight', 'PleurisyLeft',
+              'PleurisyRight']
+    writer = csv.DictWriter(out, fieldnames=fields)
+    writer.writeheader()
+    for file in os.listdir(paths[1]):
+        location = [paths[1], paths[1].replace("Left", "Right")]
+        test_data = [[[], [], []], [[], [], []]]
+        for i in range(len(location)):
+            for dirs in os.listdir(os.path.join(location[i], file)):
+                region = 0 if dirs == 'FrontBack' else 1 if dirs == 'LeftRight' else 2
+                _path = os.path.join(location[i], file, dirs)
+                for _file in os.listdir(_path):
+                    img = image.img_to_array(image.load_img(os.path.join(_path, _file), target_size=(32, 32)))
+                    test_data[i][region].append(img)
+        for i in range(2):
+            for j in range(3):
+                test_data[i][j] = np.array(test_data[i][j], dtype='float32') / 255.0
+                m = test_data[i][j].shape[0]
+                test_data[i][j] = test_data[i][j].reshape(m, -1)
+        results = [[[[], [], []], [[], [], []], [[], [], []]], [[[], [], []], [[], [], []], [[], [], []]]]
+        for i in range(2):
+            for j in range(3):
+                for k in range(3):
+                    results[i][j][k] = svm_array[i][j][k].predict(test_data[i][j])
+        final_result = [[[], [], []], [[], [], []]]
+        for i in range(2):
+            for j in range(3):
+                final_result[i][j] = np.round(np.average([np.count_nonzero(arr == '1') / (
+                        np.count_nonzero(arr == '1') + np.count_nonzero(arr == '0')) for arr in results[i][j]]), 2)
+        writer.writerow(
+            {'Filename': file, 'LeftLungAffected': final_result[0][0], 'RightLungAffected': final_result[1][0],
+             'CavernsLeft': final_result[0][1], 'CavernsRight': final_result[1][1],
+             'PleurisyLeft': final_result[0][2], 'PleurisyRight': final_result[1][2]})
+
+
+def driver():
+    print(datetime.datetime.now())
+    create_arrays(paths, separate(count()))
+    run(saved=True)
+    print(datetime.datetime.now())
+
+
+driver()
